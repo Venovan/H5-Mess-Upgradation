e36337f952c3ac11a65ee2580cd199d653e55463
Himanshu Choudhary <22himanshu14@gmail.com>
2022-12-11 10:05:45 +0530

    Merged Changes

--- Backend/mess/models.py
+++ Backend/mess/models.py
@@ -7,30 +7,32 @@ MEAL_CHOICES = [('B', 'Breakfast'),
                 ('S', 'Snacks'),
                 ('D', 'Dinner')]
 
-STATUS = [  ('A', "Allowed"), 
-            ('NA', "Not Allowed")]
+STATUS = [('A', "Allowed"),
+          ('NA', "Not Allowed")]
 
 
-MSG_LEVELS = [  ("warning", 'Warning'),
-                ("alert", "Alert"),
-                ("notify", "Notification"),
-                ("info", "Information")]
+MSG_LEVELS = [("warning", 'Warning'),
+              ("alert", "Alert"),
+              ("notify", "Notification"),
+              ("info", "Information")]
 
 
 def ID_valid(value):
     if (len(value) != 9):
-        raise ValidationError(("%(value)s is invalid"), params={"value": value})
+        raise ValidationError(("%(value)s is invalid"),
+                              params={"value": value})
     else:
         return value
+
 
 def image_handler(instance, filename):
     ext = filename.split('.')[-1]
     return "/".join(["photos", '{}.{}'.format(instance.rollNumber, ext)])
 
+
 def menu_handler(instance, filename):
     ext = filename.split('.')[-1]
     return "/".join(["menu", '{}.{}'.format(str(instance.start.day) + str(instance.start.strftime('%b')), ext)])
-    
 
 
 # Create your models here.
@@ -38,17 +40,17 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     alias = models.CharField(max_length=10, blank=True)
     permission = models.CharField(max_length=10, choices=STATUS, default='NA')
-    rollNumber = models.CharField(max_length=12, validators=[ID_valid], unique=True)
+    rollNumber = models.CharField(
+        max_length=12, validators=[ID_valid], unique=True)
     roomNumber = models.CharField(max_length=6)
     RFID = models.CharField(max_length=15, blank=True, unique=False, null=True)
-    photo = models.ImageField(upload_to=image_handler, default='default.png')
+    photo = models.ImageField(upload_to=image_handler, default='avatar.jpg')
 
     class Meta:
         ordering = ["rollNumber"]
 
     def __str__(self):
         return self.name
-
 
 
 class Meal(models.Model):
@@ -64,18 +66,16 @@ class Meal(models.Model):
         return self.student.rollNumber + "/" + str(self.date) + "/" + self.type + "/" + self.weight
 
 
-
 class Announcement(models.Model):
     heading = models.CharField(max_length=100)
     issueDate = models.DateField()
     body = models.TextField(blank=True)
     display = models.BooleanField(default=False)
     link = models.URLField(max_length=200, blank=True)
-    level = models.CharField(max_length = 12, choices=MSG_LEVELS, default="info")
+    level = models.CharField(max_length=12, choices=MSG_LEVELS, default="info")
 
     def __str__(self):
         return self.level + str(self.pk)
-
 
 
 class Menu(models.Model):
@@ -83,7 +83,7 @@ class Menu(models.Model):
     start = models.DateField(auto_now=False, default=now)
 
     def __str__(self):
-        return str(self.start.day) + str(self.start.strftime('%b')) 
-    
+        return str(self.start.day) + str(self.start.strftime('%b'))
+
     class Meta:
         ordering = ["start"]
--- Backend/mess/views.py
+++ Backend/mess/views.py
@@ -6,6 +6,9 @@ from rest_framework import status
 from datetime import datetime, timedelta
 from django.db.models import Avg, Sum, Variance
 import numpy as np
+from H5Mess import settings
+import requests
+import base64
 
 today = str(datetime.now().date())
 hours = datetime.now().hour
@@ -27,6 +30,60 @@ else:
 
 PIN_CODES = [None, None, None]
 ROLL_WAITING = [None, None, None]
+
+
+@api_view(['POST'])
+def sso_login(request):
+    access_code = request.data.get("access_code")
+    print("ACCESS CODE", access_code)
+    token_exchange_response = requests.post(url=settings.TOKEN_EXCHANGE_URL,
+                                            headers={
+                                                "Authorization": "Basic {}".format(base64.b64encode("{}:{}".format(settings.CLIENT_ID, settings.CLIENT_SECRET).encode('ascii')).decode('ascii')),
+                                                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                            },
+                                            data="code={}&redirect_uri={}&grant_type=authorization_code".format(access_code, settings.REDIRECT_URI),)
+    token_exchanage = token_exchange_response.json()
+    print("TOKEN EXCHANGE RESPONSE", token_exchanage)
+    access_token = token_exchanage.get("access_token")
+    resources_response = requests.get(url=settings.RESOURCES_URL, headers={
+        "Authorization": "Bearer {}".format(access_token),
+    },)
+    resources = resources_response.json()
+    print(resources)
+    roll_number = resources.get("roll_number")
+    name = "{} {}".format(resources.get("first_name"),
+                          resources.get("last_name"))
+    student, _ = Student.objects.get_or_create(
+        name=name, rollNumber=roll_number)
+    result = StudentSerializer(student, context={"request": request})
+    return Response(result.data)
+
+
+@api_view(['POST'])
+def update(request):
+    rollNumber = request.data.get("rollNumber")
+    roomNumber = request.data.get("roomNumber")
+    print(rollNumber, roomNumber)
+    student = Student.objects.get(rollNumber=rollNumber)
+    student.roomNumber = roomNumber
+    if len(dict(request.FILES)) > 0 and "file" in dict(request.FILES).keys():
+        print("CONTAINS IMAGE")
+        student.photo = request.FILES["file"]
+    student.save()
+    return Response({
+        "messge": "Updated successfully",
+    }, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_student(request):
+    roll_number = request.headers.get("rollNumber")
+    if Student.objects.filter(rollNumber=roll_number).exists():
+        student = Student.objects.get(rollNumber=roll_number)
+        result = StudentSerializer(student, context={"request": request})
+        return Response(result.data)
+    else:
+        return Response({"message": "No student with the matching roll number"})
 
 
 @api_view(['GET', 'PATCH'])
@@ -59,7 +116,7 @@ def login(request, rfid_pin):
             except:
                 RFID = None
             if RFID == None:
-                RFID =  ROLL_WAITING[1]
+                RFID = ROLL_WAITING[1]
                 ROLL_WAITING[1] = None
             if RFID != None:
                 try:
@@ -69,17 +126,14 @@ def login(request, rfid_pin):
                     elif (Meal.objects.filter(student=student, type=MEAL_TYPE, date=datetime.now().date()).exists()):
                         return Response(student.name, status=status.HTTP_208_ALREADY_REPORTED)
                     else:
-                        meal = Meal(student=student, type=MEAL_TYPE, weight=None, date=datetime.now().date())
+                        meal = Meal(student=student, type=MEAL_TYPE,
+                                    weight=None, date=datetime.now().date())
                         meal.save()
                         return Response(student.name, status=status.HTTP_201_CREATED)
                 except Student.DoesNotExist:
                     return Response(status=status.HTTP_404_NOT_FOUND)
             else:
                 return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
 
 
 @api_view(['GET'])
@@ -95,12 +149,13 @@ def weight(request, rfid_pin):
             except:
                 RFID = None
             if RFID == None:
-                RFID =  ROLL_WAITING[2]
+                RFID = ROLL_WAITING[2]
                 ROLL_WAITING[2] = None
             if RFID != None:
                 if (Student.objects.filter(RFID=RFID).exists()):
                     try:
-                        meal = Meal.objects.get(student__RFID=RFID, type=MEAL_TYPE, date=datetime.now().date())
+                        meal = Meal.objects.get(
+                            student__RFID=RFID, type=MEAL_TYPE, date=datetime.now().date())
                         if (meal.weight == None):
                             return Response(RFID, status=status.HTTP_202_ACCEPTED)
                         else:
@@ -113,18 +168,20 @@ def weight(request, rfid_pin):
                 return Response(status=status.HTTP_204_NO_CONTENT)
 
         elif rfid_pin == "update":
-            meal = Meal.objects.get(student__RFID=request.GET["rfid"], type=MEAL_TYPE, date=datetime.now().date())
+            meal = Meal.objects.get(
+                student__RFID=request.GET["rfid"], type=MEAL_TYPE, date=datetime.now().date())
             meal.weight = request.GET["weight"]
             meal.save()
             return Response(Student.objects.get(RFID-RFID).name, status=status.HTTP_423_LOCKED)
-        
+
 
 @api_view(['GET', 'POST'])
 def app(request, call):
     if request.method == 'GET':
         if call == "validate":
             if PIN_CODES[int(request.data.get("machine"))] == request.data.get("code"):
-                ROLL_WAITING[int(request.data.get("machine"))] = request.data.get("rfid")
+                ROLL_WAITING[int(request.data.get("machine"))
+                             ] = request.data.get("rfid")
                 return Response(status=status.HTTP_202_ACCEPTED)
             else:
                 return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
@@ -144,7 +201,8 @@ def app(request, call):
                 student = Student.objects.get(rollNumber=call)
             except Student.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
-            serializer = StudentSerializer(student)
+            serializer = StudentSerializer(
+                student, context={"request": request})
             return Response(serializer.data)
 
     elif request.method == "POST":
@@ -163,7 +221,8 @@ def arena(request, call):
         interval = request.data.get("interval")
         upto = upto if upto != None else today
         interval = interval if interval != None else "0"
-        end = datetime(int(upto.split("/")[0]), int(upto.split("/")[1]), int(upto.split("/")[2]))
+        end = datetime(
+            int(upto.split("/")[0]), int(upto.split("/")[1]), int(upto.split("/")[2]))
         start = end - timedelta(days=int(interval))
         mealtype = request.data.get("type")
         student = request.data.get("rollNumber")
@@ -177,13 +236,12 @@ def arena(request, call):
         #     return Response(variance_date_waste(start, end, mealtype))
         elif call == "movingAvg":
             return Response(moving_avg_waste(start, end, mealtype))
-        elif call == "myStats":       #datewise wastage
+        elif call == "myStats":  # datewise wastage
             return Response(student_days_total(student, start, end, mealtype, operation))
-        elif call == "percentile":    #percentile among low wastage
-            return Response(percentile(student ,mealtype))
-        elif call == "topN":   #leaderboard
+        elif call == "percentile":  # percentile among low wastage
+            return Response(percentile(student, mealtype))
+        elif call == "topN":  # leaderboard
             return Response(top_N_scorers(N, start, end, mealtype))
-
 
 
 # OVERALL STATISTICS API CALLS
@@ -194,11 +252,13 @@ def total_day_waste(start, end, type=None):
     else:
         return Meal.objects.filter(date__range=[start, end], type=type).aggregate(Sum("weight"))
 
+
 def average_day_waste(start, end, type=None):
     if type == None:
         return Meal.objects.filter(date_range=[start, end]).aggregate(Avg("weight"))
     else:
         return Meal.objects.filter(date__range=[start, end], type=type).aggregate(Avg("weight"))
+
 
 def variance_date_waste(start, end, type=None):
     if type == None:
@@ -215,47 +275,52 @@ def moving_avg_waste(start, end, type=None):
 
 
 # INDIVIDUAL STATISTICS API CALLS
- 
-#returns total food wasted for last x days upto given date "upto"
+
+# returns total food wasted for last x days upto given date "upto"
 def student_days_total(RL, start, end, type=None, operation="Sum"):
     if RL == None:
         return status.HTTP_400_BAD_REQUEST
-    if type==None:
-        meals =  Meal.objects.filter(student__rollNumber=RL, date__range = [start, end])
+    if type == None:
+        meals = Meal.objects.filter(
+            student__rollNumber=RL, date__range=[start, end])
         if operation == "Sum":
             return meals.aggregate(Sum("weight"))
-        elif operation =="Avg":
+        elif operation == "Avg":
             return meals.aggregate(Avg("weight"))
     else:
-        meals =  Meal.objects.filter(student__rollNumber= RL, type=type, date__range = [start, end])
+        meals = Meal.objects.filter(
+            student__rollNumber=RL, type=type, date__range=[start, end])
         if operation == "Sum":
             return meals.aggregate(Sum("weight"))
-        elif operation =="Avg":
+        elif operation == "Avg":
             return meals.aggregate(Avg("weight"))
 
-def percentile(id,type=None):
-    students = Meal.objects.all().values('student_id').annotate(Avg("weight")).order_by("weight__avg") 
-     
+
+def percentile(id, type=None):
+    students = Meal.objects.all().values('student_id').annotate(
+        Avg("weight")).order_by("weight__avg")
+
     totalstudents = students.count()
-   
+
     myavg = students.filter(student_id=id).values_list("weight__avg")
     studentswithmorewaste = students.filter(weight__avg__gte=myavg).count()
 
     percentile = (studentswithmorewaste)*100.0/totalstudents
-    
+
     return percentile
 
+
 def top_N_scorers(N, start, end, type=None):
-    if type==None:
-        leaders =  Meal.objects.filter(date__range=[start, end]).values('student__rollNumber', "student__name").annotate(Avg("weight")).order_by("weight__avg") 
+    if type == None:
+        leaders = Meal.objects.filter(date__range=[start, end]).values(
+            'student__rollNumber', "student__name").annotate(Avg("weight")).order_by("weight__avg")
     else:
-        leaders = Meal.objects.filter(date__range=[start, end], type=type).values('student__rollNumber').annotate(Avg("weight")).order_by("weight__avg") 
-    
+        leaders = Meal.objects.filter(date__range=[start, end], type=type).values(
+            'student__rollNumber').annotate(Avg("weight")).order_by("weight__avg")
+
     N = len(leaders) if N == None else min(len(leaders), int(N))
-    
+
     for each in range(N):
         del leaders[each]["student__rollNumber"]
-    
-    return leaders[:N]
 
-    
+    return leaders[:N]
